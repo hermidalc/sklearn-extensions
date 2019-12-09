36c5a47ca1bd1848fc384266d9b758d61ef58aad
Leandro Hermida <hermidal@cs.umd.edu>
2019-12-09 15:55:49 -0500

    Undo adding CachedSVC

--- svm/cached.py
+++ svm/cached.py
@@ -1,4 +1,4 @@
-from sklearn.svm import LinearSVC, SVC
+from sklearn.svm import LinearSVC
 from ..cached import CachedFitMixin
 
 
@@ -14,20 +14,3 @@ class CachedLinearSVC(CachedFitMixin, LinearSVC):
                          intercept_scaling=intercept_scaling,
                          class_weight=class_weight, verbose=verbose,
                          random_state=random_state, max_iter=max_iter)
-
-
-class CachedSVC(CachedFitMixin, SVC):
-
-    def __init__(self, memory, C=1.0, kernel='rbf', degree=3, gamma='scale',
-                 coef0=0.0, shrinking=True, probability=False, tol=1e-3,
-                 cache_size=200, class_weight=None, verbose=False, max_iter=-1,
-                 decision_function_shape='ovr', break_ties=False,
-                 random_state=None):
-        self.memory = memory
-        super().__init__(C=C, kernel=kernel, degree=degree, gamma=gamma,
-                         coef0=coef0, shrinking=shrinking,
-                         probability=probability, tol=tol,
-                         cache_size=cache_size, class_weight=class_weight,
-                         verbose=verbose, max_iter=max_iter,
-                         decision_function_shape=decision_function_shape,
-                         break_ties=break_ties, random_state=random_state)
