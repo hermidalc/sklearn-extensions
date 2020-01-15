a211df0f2b00f456c26c5b46e1827b0b76f49daa
Leandro Hermida <hermidal@cs.umd.edu>
2020-01-15 13:30:12 -0500

    ensemble code updates for sklearn 0.22.1

--- ensemble/cached.py
+++ ensemble/cached.py
@@ -12,8 +12,9 @@ class CachedGradientBoostingClassifier(CachedFitMixin,
                  min_weight_fraction_leaf=0., max_depth=3,
                  min_impurity_decrease=0., min_impurity_split=None, init=None,
                  random_state=None, max_features=None, verbose=0,
-                 max_leaf_nodes=None, warm_start=False, presort='auto',
-                 validation_fraction=0.1, n_iter_no_change=None, tol=1e-4):
+                 max_leaf_nodes=None, warm_start=False, presort='deprecated',
+                 validation_fraction=0.1, n_iter_no_change=None, tol=1e-4,
+                 ccp_alpha=0.0):
         self.memory = memory
         super().__init__(loss=loss, learning_rate=learning_rate,
                          n_estimators=n_estimators, criterion=criterion,
@@ -27,18 +28,19 @@ class CachedGradientBoostingClassifier(CachedFitMixin,
                          min_impurity_split=min_impurity_split,
                          warm_start=warm_start, presort=presort,
                          validation_fraction=validation_fraction,
-                         n_iter_no_change=n_iter_no_change, tol=tol)
+                         n_iter_no_change=n_iter_no_change, tol=tol,
+                         ccp_alpha=ccp_alpha)
 
 
 class CachedRandomForestClassifier(CachedFitMixin, RandomForestClassifier):
 
-    def __init__(self, memory, n_estimators='warn', criterion='gini',
+    def __init__(self, memory, n_estimators=100, criterion='gini',
                  max_depth=None, min_samples_split=2, min_samples_leaf=1,
                  min_weight_fraction_leaf=0., max_features='auto',
                  max_leaf_nodes=None, min_impurity_decrease=0.,
                  min_impurity_split=None, bootstrap=True, oob_score=False,
                  n_jobs=None, random_state=None, verbose=0, warm_start=False,
-                 class_weight=None):
+                 class_weight=None, ccp_alpha=0.0, max_samples=None):
         self.memory = memory
         super().__init__(n_estimators=n_estimators, criterion=criterion,
                          max_depth=max_depth,
@@ -52,18 +54,19 @@ class CachedRandomForestClassifier(CachedFitMixin, RandomForestClassifier):
                          bootstrap=bootstrap, oob_score=oob_score,
                          n_jobs=n_jobs, random_state=random_state,
                          verbose=verbose, warm_start=warm_start,
-                         class_weight=class_weight)
+                         class_weight=class_weight, ccp_alpha=ccp_alpha,
+                         max_samples=max_samples)
 
 
 class CachedExtraTreesClassifier(CachedFitMixin, ExtraTreesClassifier):
 
-    def __init__(self, memory, n_estimators='warn', criterion='gini',
+    def __init__(self, memory, n_estimators=100, criterion='gini',
                  max_depth=None, min_samples_split=2, min_samples_leaf=1,
                  min_weight_fraction_leaf=0., max_features='auto',
                  max_leaf_nodes=None, min_impurity_decrease=0.,
                  min_impurity_split=None, bootstrap=False, oob_score=False,
                  n_jobs=None, random_state=None, verbose=0, warm_start=False,
-                 class_weight=None):
+                 class_weight=None, ccp_alpha=0.0, max_samples=None):
         self.memory = memory
         super().__init__(n_estimators=n_estimators, criterion=criterion,
                          max_depth=max_depth,
@@ -77,4 +80,5 @@ class CachedExtraTreesClassifier(CachedFitMixin, ExtraTreesClassifier):
                          bootstrap=bootstrap, oob_score=oob_score,
                          n_jobs=n_jobs, random_state=random_state,
                          verbose=verbose, warm_start=warm_start,
-                         class_weight=class_weight)
+                         class_weight=class_weight, ccp_alpha=ccp_alpha,
+                         max_samples=max_samples)
