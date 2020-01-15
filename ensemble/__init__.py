143b71185532b9a8f8aa1d343742da9a175b404e
Leandro Hermida <hermidal@cs.umd.edu>
2020-01-15 13:31:34 -0500

    ensemble file name changes for sklearn 0.22.1

--- ensemble/__init__.py
+++ ensemble/__init__.py
@@ -3,7 +3,7 @@ The :mod:`sklearn.ensemble` module includes ensemble-based methods for
 classification, regression and anomaly detection.
 """
 
-from .cached import (
+from ._cached import (
     CachedExtraTreesClassifier, CachedGradientBoostingClassifier,
     CachedRandomForestClassifier)
 
