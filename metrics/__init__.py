c4797cff995f0fcfc2e2bcc6225c790e26660f25
Leandro Hermida <hermidal@cs.umd.edu>
2020-01-15 14:25:26 -0500

    metrics file name updates for sklearn 0.22.1

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -3,10 +3,10 @@ The :mod:`sklearn.metrics` module includes score functions, performance metrics
 and pairwise metrics and distance computations.
 """
 
-from .scorer import check_scoring
-from .scorer import make_scorer
-from .scorer import SCORERS
-from .scorer import get_scorer
+from ._scorer import check_scoring
+from ._scorer import make_scorer
+from ._scorer import SCORERS
+from ._scorer import get_scorer
 
 
 __all__ = ['check_scoring',
