2a9028026eee85bb57cd164a8de7311d2fde349f
Leandro Hermida <hermidal@cs.umd.edu>
2020-03-01 16:13:03 -0500

    New ShiftedLogTransformer class to support loky

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -5,11 +5,13 @@ normalization, binarization methods.
 
 from ._batch_effect import (LimmaBatchEffectRemover, stICABatchEffectRemover,
                             SVDBatchEffectRemover)
+from ._data import ShiftedLogTransformer
 from ._rna_seq import DESeq2RLEVST, EdgeRTMMLogCPM
 
 
 __all__ = ['DESeq2RLEVST',
            'EdgeRTMMLogCPM',
            'LimmaBatchEffectRemover',
+           'ShiftedLogTransformer',
            'stICABatchEffectRemover',
            'SVDBatchEffectRemover']
---
+++ preprocessing/_data.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from sklearn.preprocessing import FunctionTransformer
+
+
+def shifted_log(X, base, shift):
+    return np.log(X + shift) / np.log(base)
+
+
+class ShiftedLogTransformer(FunctionTransformer):
+    """Shifted log transformer
+
+    Parameters
+    ----------
+    base : int (default = 2)
+        Base to use when taking logarithm
+
+    shift : float (default = 1.0)
+        Value to shift data by before taking logarithm
+    """
+
+    def __init__(self, base=2, shift=1):
+        self.base = base
+        self.shift = shift
+        super().__init__(func=shifted_log, check_inverse=False,
+                         kw_args={'base': base, 'shift': shift}, validate=True)
