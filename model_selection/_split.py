2b48c8b1f12df46b5d1df9680515f2147e4e9f6d
Leandro Hermida <hermidal@cs.umd.edu>
2020-05-30 18:54:29 -0400

    Improvements to cv iterators

--- model_selection/_split.py
+++ model_selection/_split.py
@@ -8,8 +8,11 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import StratifiedKFold, StratifiedShuffleSplit
-from sklearn.model_selection._split import _BaseKFold, _RepeatedSplits
-from sklearn.utils.validation import check_array, check_random_state, indexable
+from sklearn.model_selection._split import (_BaseKFold, _RepeatedSplits,
+                                            _validate_shuffle_split)
+from sklearn.utils import _approximate_mode
+from sklearn.utils.validation import (check_array, check_random_state,
+                                      indexable, _num_samples)
 
 
 class StratifiedGroupKFold(_BaseKFold):
@@ -199,6 +202,8 @@ class RepeatedStratifiedGroupKFold(_RepeatedSplits):
 class StratifiedSampleFromGroupKFold(StratifiedKFold):
 
     def split(self, X, y, groups):
+        if groups is None:
+            raise ValueError('The groups parameter should not be None')
         X, y, groups = indexable(X, y, groups)
         indices = pd.DataFrame(groups).groupby(0).apply(
             pd.DataFrame.sample, n=1, random_state=self.random_state
@@ -305,9 +310,8 @@ class StratifiedGroupShuffleSplit(StratifiedShuffleSplit):
         self._default_test_size = 0.1
 
     def _iter_indices(self, X, y, groups):
-        y = check_array(y, ensure_2d=False, dtype=None)
         if groups is None:
-            raise ValueError("The 'groups' parameter should not be None.")
+            raise ValueError('The groups parameter should not be None')
         groups = check_array(groups, ensure_2d=False, dtype=None)
         (unique_groups, unique_groups_y), group_indices = np.unique(
             np.stack((groups, y)), axis=1, return_inverse=True)
@@ -325,14 +329,75 @@ class StratifiedGroupShuffleSplit(StratifiedShuffleSplit):
 
 class StratifiedSampleFromGroupShuffleSplit(StratifiedShuffleSplit):
 
-    def split(self, X, y, groups):
-        X, y, groups = indexable(X, y, groups)
-        indices = pd.DataFrame(groups).groupby(0).apply(
-            pd.DataFrame.sample, n=1, random_state=self.random_state
-            ).reset_index(level=0, drop=True).sort_index().index.values
-        X = X.iloc[indices] if hasattr(X, 'iloc') else X[indices]
-        y = y[indices]
-        for train, test in self._iter_indices(X, y):
+    def _iter_indices(self, X, y, groups):
+        if groups is None:
+            raise ValueError('The groups parameter should not be None')
+
+        rng = check_random_state(self.random_state)
+
+        for _ in range(self.n_splits):
+            indices = pd.DataFrame(groups).groupby(0).apply(
+                pd.DataFrame.sample, n=1, random_state=rng
+                ).reset_index(level=0, drop=True).sort_index().index.values
+            Xr = X.iloc[indices] if hasattr(X, 'iloc') else X[indices]
+            yr = y[indices]
+
+            n_samples = _num_samples(Xr)
+            n_train, n_test = _validate_shuffle_split(
+                n_samples, self.test_size, self.train_size,
+                default_test_size=self._default_test_size)
+
+            if yr.ndim == 2:
+                # for multi-label y, map each distinct row to a string repr
+                # using join because str(row) uses an ellipsis if
+                # len(row) > 1000
+                yr = np.array([' '.join(row.astype('str')) for row in yr])
+
+            classes, y_indices = np.unique(yr, return_inverse=True)
+            n_classes = classes.shape[0]
+
+            class_counts = np.bincount(y_indices)
+            if np.min(class_counts) < 2:
+                raise ValueError("The least populated class in y has only 1"
+                                 " member, which is too few. The minimum"
+                                 " number of groups for any class cannot"
+                                 " be less than 2.")
+
+            if n_train < n_classes:
+                raise ValueError('The train_size = %d should be greater or '
+                                 'equal to the number of classes = %d' %
+                                 (n_train, n_classes))
+            if n_test < n_classes:
+                raise ValueError('The test_size = %d should be greater or '
+                                 'equal to the number of classes = %d' %
+                                 (n_test, n_classes))
+
+            # Find the sorted list of instances for each class:
+            # (np.unique above performs a sort, so code is O(n logn) already)
+            class_indices = np.split(np.argsort(y_indices, kind='mergesort'),
+                                     np.cumsum(class_counts)[:-1])
+
+            # if there are ties in the class-counts, we want
+            # to make sure to break them anew in each iteration
+            n_i = _approximate_mode(class_counts, n_train, rng)
+            class_counts_remaining = class_counts - n_i
+            t_i = _approximate_mode(class_counts_remaining, n_test, rng)
+
+            train = []
+            test = []
+
+            for i in range(n_classes):
+                permutation = rng.permutation(class_counts[i])
+                perm_indices_class_i = class_indices[i].take(permutation,
+                                                             mode='clip')
+
+                train.extend(perm_indices_class_i[:n_i[i]])
+                test.extend(perm_indices_class_i[n_i[i]:n_i[i] + t_i[i]])
+
+            train = rng.permutation(train)
+            test = rng.permutation(test)
+
             train_index = indices[train]
             test_index = indices[test]
+
             yield train_index, test_index
