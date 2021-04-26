8341ad341f39f9ae060eac9281252cb011d6ccff
Leandro Hermida <hermidal@cs.umd.edu>
2021-04-26 14:51:08 -0400

    Add EdgeRTMMTPM to __init__

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -7,11 +7,12 @@ from ._batch_effect import (LimmaBatchEffectRemover, stICABatchEffectRemover,
                             SVDBatchEffectRemover)
 from ._data import LogTransformer
 from ._nanostring import NanoStringNormalizer, NanoStringDiffNormalizer
-from ._rna_seq import DESeq2RLEVST, EdgeRTMMLogCPM
+from ._rna_seq import DESeq2RLEVST, EdgeRTMMLogCPM, EdgeRTMMTPM
 
 
 __all__ = ['DESeq2RLEVST',
            'EdgeRTMMLogCPM',
+           'EdgeRTMMTPM',
            'LimmaBatchEffectRemover',
            'LogTransformer',
            'NanoStringNormalizer',
