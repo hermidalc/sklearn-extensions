02b68d18c7b3279e9bc284a85bf6717e8dd6c129
Leandro Hermida <softdev@leandrohermida.com>
2022-06-23 18:42:12 -0400

    Update code to sklearn 1.1 and Black formatting

--- svm/__init__.py
+++ svm/__init__.py
@@ -4,5 +4,4 @@ The :mod:`sklearn.svm` module includes Support Vector Machine algorithms.
 
 from ._cached import CachedLinearSVC, CachedSVC
 
-__all__ = ['CachedLinearSVC',
-           'CachedSVC']
+__all__ = ["CachedLinearSVC", "CachedSVC"]
--- svm/_cached.py
+++ svm/_cached.py
@@ -3,31 +3,76 @@ from ..cached import CachedFitMixin
 
 
 class CachedLinearSVC(CachedFitMixin, LinearSVC):
-
-    def __init__(self, memory, penalty='l2', loss='squared_hinge', dual=True,
-                 tol=1e-2, C=1.0, multi_class='ovr', fit_intercept=True,
-                 intercept_scaling=1, class_weight=None, verbose=0,
-                 random_state=None, max_iter=1000):
-        super().__init__(penalty=penalty, loss=loss, dual=dual, tol=tol, C=C,
-                         multi_class=multi_class, fit_intercept=fit_intercept,
-                         intercept_scaling=intercept_scaling,
-                         class_weight=class_weight, verbose=verbose,
-                         random_state=random_state, max_iter=max_iter)
+    def __init__(
+        self,
+        memory,
+        *,
+        penalty="l2",
+        loss="squared_hinge",
+        dual=True,
+        tol=1e-4,
+        C=1.0,
+        multi_class="ovr",
+        fit_intercept=True,
+        intercept_scaling=1,
+        class_weight=None,
+        verbose=0,
+        random_state=None,
+        max_iter=1000,
+    ):
+        super().__init__(
+            penalty=penalty,
+            loss=loss,
+            dual=dual,
+            tol=tol,
+            C=C,
+            multi_class=multi_class,
+            fit_intercept=fit_intercept,
+            intercept_scaling=intercept_scaling,
+            class_weight=class_weight,
+            verbose=verbose,
+            random_state=random_state,
+            max_iter=max_iter,
+        )
         self.memory = memory
 
 
 class CachedSVC(CachedFitMixin, SVC):
-
-    def __init__(self, memory, C=1.0, kernel='rbf', degree=3, gamma='scale',
-                 coef0=0.0, shrinking=True, probability=False, tol=1e-3,
-                 cache_size=200, class_weight=None, verbose=False, max_iter=-1,
-                 decision_function_shape='ovr', break_ties=False,
-                 random_state=None):
-        super().__init__(C=C, kernel=kernel, degree=degree, gamma=gamma,
-                         coef0=coef0, shrinking=shrinking,
-                         probability=probability, tol=tol,
-                         cache_size=cache_size, class_weight=class_weight,
-                         verbose=verbose, max_iter=max_iter,
-                         decision_function_shape=decision_function_shape,
-                         break_ties=break_ties, random_state=random_state)
+    def __init__(
+        self,
+        memory,
+        *,
+        C=1.0,
+        kernel="rbf",
+        degree=3,
+        gamma="scale",
+        coef0=0.0,
+        shrinking=True,
+        probability=False,
+        tol=1e-3,
+        cache_size=200,
+        class_weight=None,
+        verbose=False,
+        max_iter=-1,
+        decision_function_shape="ovr",
+        break_ties=False,
+        random_state=None,
+    ):
+        super().__init__(
+            C=C,
+            kernel=kernel,
+            degree=degree,
+            gamma=gamma,
+            coef0=coef0,
+            shrinking=shrinking,
+            probability=probability,
+            tol=tol,
+            cache_size=cache_size,
+            class_weight=class_weight,
+            verbose=verbose,
+            max_iter=max_iter,
+            decision_function_shape=decision_function_shape,
+            break_ties=break_ties,
+            random_state=random_state,
+        )
         self.memory = memory
