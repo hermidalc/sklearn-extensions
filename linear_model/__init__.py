bbc68ffe017d08fccd153028f8cdc7ab40d8de4c
Leandro Hermida <softdev@leandrohermida.com>
2022-06-23 16:56:42 -0400

    Black formatting

--- linear_model/__init__.py
+++ linear_model/__init__.py
@@ -4,4 +4,4 @@ The :mod:`sklearn.linear_model` module implements a variety of linear models.
 
 from ._cached import CachedLogisticRegression
 
-__all__ = ['CachedLogisticRegression']
+__all__ = ["CachedLogisticRegression"]
